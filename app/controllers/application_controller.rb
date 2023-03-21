e5f4a97b42a68fa247faae0e8c79c075f8ef7f9c
Alejandra Landa <aleriquene@gmail.com>
2023-03-21 17:30:37 +0000

    more work on results

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,9 @@ class ApplicationController < ActionController::Base
 def blank_square_form
   render({:template=>"calculation_templates/square_form.html.erb"})
 end
+def calculate_square
+  @nums = params.fetch("elephant").to_f
+  @square_of_nums = @nums ** 2
+  render({:template=>"calculation_templates/square_results.html.erb"})
+end
 end
