7976762b9bedf339f5223304690099826b508322
Alejandra Landa <aleriquene@gmail.com>
2023-03-22 16:10:50 +0000

    random created

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,6 +10,7 @@ end
 def calculate_random
   @lower = params.fetch("user_min").to_f
   @upper = params.fetch("user_max").to_f
+  @result = rand(@lower..@upper)
   render({:template=>"calculation_templates/rand_results.html.erb"})
 
 end
@@ -17,7 +18,28 @@ def calculate_square_root
   render({:template => "calculation_templates/square_root_form.html.erb"})
 end
 def square_root_results
-  
+   @num = params.fetch("user_number").to_f
+   @squareroot = Math.sqrt(@num)
   render({:template => "calculation_templates/square_root_results.html.erb"})
+end
+def new_payment
+  render({:template=>"calculation_templates/new_payment.html.erb"})
+end
+def payment_results
+  @apr = params.fetch("user_apr")
+  # @finalapr = @apr."%.4f"
+  @years = params.fetch("user_years")
+  @payment = params.fetch("user_pv")
+  
+  render({:template=>"calculation_templates/payment_results.html.erb"})
+end
+def random_number
+  render({:template=> "calculation_templates/random_new.html.erb"})
+end
+def random_results
+  @usermin = param.fetch("user_min").to_f
+  @usermax = param.fetch("user_max").to_f
+  @randomresult = rand(@usermin..@usermax)
+  render({:template=>"calculation_templates/random_results.html.erb"})
 end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,8 @@ get("/square/results",{:controller => "application", :action=>"calculate_square"
 get("/random/results",{:controller => "application", :action=>"calculate_random"})
 get("/square_root/new",{:controller => "application", :action=>"calculate_square_root"})
 get("/square_root/results",{:controller => "application", :action=>"square_root_results"})
+get("/payment/new",{:controller => "application", :action=>"new_payment"})
+get("/payment/results",{:controller => "application", :action=>"payment_results"})
+get("/random/new",{:controller => "application", :action=>"random_number"})
+get("/random/results",{:controller=>"application", :action=>"random_results"})
 end
